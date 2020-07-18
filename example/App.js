421554277e0f46929776ad5d0b438d1157f08adc
Alex Thomas <hello@cawfree.com>
2020-07-18 04:06:39 +0100

    chore: update example

--- example/App.js
+++ example/App.js
@@ -3,12 +3,23 @@ import PropTypes from "prop-types";
 import { SafeAreaView, StyleSheet, View } from "react-native";
 import SegmentedTextInput from "react-native-segmented-text-input";
 
+const styles = StyleSheet.create({
+  container: { padding: 15 },
+});
+
 export default () => {
   const [value, onChange] = useState(['', []]);
   return (
-    <SegmentedTextInput
-      value={value}
-      onChange={onChange}
-    />
+    <SafeAreaView
+    >
+      <View
+        style={styles.container}
+      >
+        <SegmentedTextInput
+          value={value}
+          onChange={onChange}
+        />
+      </View>
+    </SafeAreaView>
   );
 };
